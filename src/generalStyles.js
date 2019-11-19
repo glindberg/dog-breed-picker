67d9c7fec9ee986598d46b3d32f3ab966bb170a5
glindberg <gustav.lindberg1@gmail.com>
2019-11-19 14:13:14 +0100

    Styled. But not responsive

--- src/generalStyles.js
+++ src/generalStyles.js
@@ -1,10 +1,11 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 export const SCBackground = styled.div`
   height: 100vh;
   background: rgba(0, 0, 0, 0)
     linear-gradient(20deg, rgb(200, 95, 120), rgb(190, 190, 5)) repeat scroll 0%
     0%;
-  padding-top: 10rem;
+
   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  overflow: hidden;
 `;
